6b7c3122fe1049123ce8998d1cd34ea8702efe77
yingwusu <99861764+yingwusu@users.noreply.github.com>
2023-02-22 16:57:51 +0800

    Add files via upload

---
+++ jvzi.js
@@ -0,0 +1,22 @@
+let fenshu = document.getElementById('fenshu')
+let pipeizongjie = document.getElementById('pipeizongjie')
+let tuijian = document.getElementById('tuijian')
+
+let pipeiyu = localStorage.pipeidu + '分！'
+
+fenshu.innerHTML = pipeiyu
+
+tuijian.innerHTML = localStorage.tuijian_final
+
+
+gexing = function()
+{
+    if(parseInt(localStorage.pipeidu)>60)
+    {
+        pipeizongjie.innerHTML = "富平柿饼非常适合您呀!<br>可以试试看！"
+    }
+    else
+    {
+    pipeizongjie.innerHTML = "或许您可以试试寻乌脐橙！<br>相信您不会失望！"
+    }
+}
